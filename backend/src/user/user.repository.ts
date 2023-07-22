789c20bfc33b40be7db6fba56108c41112281695
o3ojunseok <o3ojunseok@gmail.com>
2023-07-22 17:15:33 +0900

    user crud in repository

--- backend/src/user/user.repository.ts
+++ backend/src/user/user.repository.ts
@@ -1,10 +1,84 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
 import { User } from "./user.entity";
 import { DataSource, Repository } from "typeorm";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
 
 @Injectable()
 export class UserRepository extends Repository<User> {
   constructor(private dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  async findAllUser() {
+    try {
+      return await this.find();
+    } catch (err) {
+      throw new InternalServerErrorException(err);
+    }
+  }
+
+  async findOneUser(username: string) {
+    try {
+      return await this.findOne({
+        where: { username },
+      });
+    } catch (err) {
+      throw new InternalServerErrorException(err);
+    } 
+  }
+  
+  async createUser(createData: CreateUserDto) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      await queryRunner.manager.save(createData);
+      await queryRunner.commitTransaction();
+    } catch (err){
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  async updateUser(id: number, updateData: UpdateUserDto) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      await queryRunner.manager.update(User, id, updateData);
+      await queryRunner.commitTransaction();
+    } catch (err){
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  async deleteUser(id: number) {
+    const queryRunner = this.dataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      await queryRunner.manager
+      .createQueryBuilder(User, 'user')
+      .delete()
+      .from(User)
+      .where("id = :id", { id })
+      .execute()
+      
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
