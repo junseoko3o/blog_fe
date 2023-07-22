28065465ed35a42d813f15fff06acf3f3e12e30e
o3ojunseok <o3ojunseok@gmail.com>
2023-07-23 03:44:13 +0900

    [LoggerMiddleware] middleware 추가

--- backend/src/common/crypto/crypto.ts
+++ backend/src/common/crypto/crypto.ts
@@ -4,16 +4,16 @@ import { Buffer } from 'buffer';
 
 @Injectable()
 export default class CryptoAes128Gcm {
-  encryptAes128Gcm(text) {
+  encryptAes256Gcm(text) {
     const key = process.env.CRYPTO_KEY;
     const iv = Crypto.randomBytes(12);
-    const cipher = Crypto.createCipheriv('aes-128-gcm', key, iv);
+    const cipher = Crypto.createCipheriv('aes-256-gcm', key, iv);
     const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
     const tag = cipher.getAuthTag();
     return Buffer.concat([iv, encrypted, tag]).toString('base64');
   }
 
-  decryptAes128Gcm(encdata) {
+  decryptAes256Gcm(encdata) {
     if (!encdata) {
       return '';
     }
@@ -24,7 +24,7 @@ export default class CryptoAes128Gcm {
     const text = bData.subarray(12, bData.length - 16); 
     const tag = bData.subarray(bData.length - 16);
 
-    const decipher = Crypto.createDecipheriv('aes-128-gcm', key, iv);
+    const decipher = Crypto.createDecipheriv('aes-256-gcm', key, iv);
     decipher.setAuthTag(tag);
     return decipher.update(text) + decipher.final('utf8');
   }
