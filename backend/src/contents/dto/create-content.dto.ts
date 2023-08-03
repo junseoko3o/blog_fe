a82e215496c217db9a74049100d82e6255393412
o3ojunseok <o3ojunseok@gmail.com>
2023-08-04 00:58:59 +0900

    [ContentDto] add create/update dto

--- backend/src/contents/dto/create-content.dto.ts
+++ backend/src/contents/dto/create-content.dto.ts
@@ -1,1 +1,12 @@
-export class CreateContentDto {}
+import { IsNumber, IsString } from "class-validator";
+
+export class CreateContentDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  contents: string;
+
+  @IsNumber()
+  created_user_id: number;
+}
--- backend/src/contents/dto/update-content.dto.ts
+++ backend/src/contents/dto/update-content.dto.ts
@@ -1,4 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateContentDto } from './create-content.dto';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
-export class UpdateContentDto extends PartialType(CreateContentDto) {}
+export class UpdateContentDto extends PartialType(CreateContentDto) {
+  @IsString()
+  @IsOptional()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  contents: string;
+
+  @IsNumber()
+  updated_user_id: number;
+}
