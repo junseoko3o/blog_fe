98c98073029a5b5cce52b4a55cfab02b1ad6d048
o3ojunseok <o3ojunseok@gmail.com>
2023-08-05 10:00:00 +0900

    [Contents] findAll

--- backend/src/contents/contents.controller.ts
+++ backend/src/contents/contents.controller.ts
@@ -1,34 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { ContentsService } from './contents.service';
 import { CreateContentDto } from './dto/create-content.dto';
 import { UpdateContentDto } from './dto/update-content.dto';
 
 @Controller('contents')
 export class ContentsController {
-  constructor(private readonly contentsService: ContentsService) {}
+  constructor(
+    private readonly contentsService: ContentsService,
+  ) {}
 
-  @Post()
-  create(@Body() createContentDto: CreateContentDto) {
-    return this.contentsService.create(createContentDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.contentsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.contentsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateContentDto: UpdateContentDto) {
-    return this.contentsService.update(+id, updateContentDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.contentsService.remove(+id);
+  async findAllContents() {
+    return await this.contentsService.findAllContents();
   }
 }
--- backend/src/contents/contents.repository.ts
+++ backend/src/contents/contents.repository.ts
@@ -3,8 +3,12 @@ import { Content } from "./content.entity";
 import { DataSource, Repository } from "typeorm";
 
 @Injectable()
-export class UserRepository extends Repository<Content> {
+export class ContentsRepository extends Repository<Content> {
   constructor(private dataSource: DataSource) {
     super(Content, dataSource.createEntityManager());
+  }
+
+  async findAllContents() {
+    return await this.find();
   }
 }
