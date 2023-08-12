0a0f3bb6ac3eceff9845542dfd19e7e90a2a25b6
o3ojunseok <o3ojunseok@gmail.com>
2023-08-13 02:58:43 +0900

    Fix: Change JwtAccessAuthGuard canActivate type

--- backend/src/user/auth/jwt-access.guard.ts
+++ backend/src/user/auth/jwt-access.guard.ts
@@ -6,13 +6,16 @@ export class JwtAccessAuthGuard implements CanActivate {
   constructor(
     private readonly jwtService: JwtService,
   ) {}
-  async canActivate(context: ExecutionContext): Promise<any> {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     try {
       const request = context.switchToHttp().getRequest();
       const access_token = request.cookies['access_token'];
       const user = await this.jwtService.verify(access_token);
       request.user = user;
-      return user;
+
+      if (request.user) {
+        return true;
+      }
     } catch(err) {
       throw new UnauthorizedException('Invalid access token.');
     }
