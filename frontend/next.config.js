01e84b265d8ed6202d280866b07e9428e572abbf
o3ojunseok <o3ojunseok@gmail.com>
2023-08-12 20:07:36 +0900

    [Frontend]Feat: next-config설정 추가

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  output: "standalone",
+};
+
 
 module.exports = nextConfig
