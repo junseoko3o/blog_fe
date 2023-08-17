4ddd0fcc4a0c03d98cf6d4c24009b0a8e6154ee7
o3ojunseok <o3ojunseok@gmail.com>
2023-08-17 23:40:03 +0900

    Feat: axios contents list

---
+++ frontend/src/hooks/contents/interface.ts
@@ -0,0 +1,10 @@
+export interface ContentList {
+  id: number,
+  title: string,
+  contents: string,
+  user_name: string,
+  created_user_id: number,
+  updated_user_id: number,
+  created_at: Date,
+  updated_at: Date,
+}
--- frontend/src/hooks/contents/useContents.ts
+++ frontend/src/hooks/contents/useContents.ts
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import api from '../../api/api';
+import { ContentList } from './interface';
 
-export const useContent = () => {
-  
+export const useContent = async () => {
+  const response = await api.get<ContentList>('/content/list');
+  return response.data;
 }
