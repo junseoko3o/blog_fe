0626366cac5269a1c5f824a10a48683746e95911
junseok <junseok.oh@gentlepie.com>
2023-10-19 16:24:51 +0900

    Fix: 소켓 유저별로 연결

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -2,23 +2,27 @@ import styles from './lib/chat.module.css';
 import useChat from "hooks/useChat";
 
 const Chat = () => {
-  const { user, messages, message, handleInputChange, sendMessage } = useChat();
+  const { user, messages, message, handleInputChange, handleKeyPress, sendMessage } = useChat();
   return (
     <div className={styles.chatContainer}>
       <div className={styles.chatMessages}>
-          {messages.map((msg, index) => (
-              <div key={index}
-              className={`${styles.message} ${user ? styles.user : styles.response}`}
-            >
-              {msg}
-              </div>
-          ))}
+        {messages.map((msg, index) => (
+          <div
+            key={index}
+            className={`${styles.message} ${
+              msg.userId === user.id ? styles.user : styles.response
+            }`}
+          >
+            {msg.message}
+          </div>
+        ))}
       </div>
       <div className={styles.inputContainer}>
         <input
           type="text"
           value={message}
           onChange={handleInputChange}
+          onKeyPress={handleKeyPress}
           placeholder="메시지를 입력하세요..."
         />
         <button onClick={sendMessage}>전송</button>
@@ -26,4 +30,5 @@ const Chat = () => {
     </div>
   );
 };
+
 export default Chat;
