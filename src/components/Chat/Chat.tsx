e2bf8927cb18266e9f4473a2fe030f25efbd2596
o3ojunseok <o3ojunseok@gmail.com>
2023-10-26 10:00:27 +0900

    Fix: chat css 수정

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -9,19 +9,20 @@ const Chat = () => {
 
   return (
     <>
-      <div className={styles.chatContainer}>
-        <div className={styles.chatMessages}>
-          {messages.map((e, i) => (
-            <div key={i}>
-              {e.userId !== user.id && <span className={styles.userName}>{e.name}</span>}
-              <div className={`${styles.message} ${e.userId === user.id ? styles.user : styles.response}`}>
-                {e.message}
+      <div className={styles.container}>
+        <div className={styles.chatContainer}>
+          <div className={styles.chatMessages}>
+            {messages.map((e, i) => (
+              <div key={i}>
+                {e.userId !== user.id && <span className={styles.userName}>{e.name}</span>}
+                <div className={`${styles.message} ${e.userId === user.id ? styles.user : styles.response}`}>
+                  {e.message}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+          <div ref={messageEndRef}></div>
         </div>
-        <div ref={messageEndRef}></div>
-      </div>
         <div className={styles.inputContainer}>
           <input
             type="text"
@@ -30,8 +31,9 @@ const Chat = () => {
             onKeyDown={handleKeyPress}
             placeholder="메시지를 입력하세요..."
           />
-          <button onClick={sendMessage}>전송</button>
+          <button className={styles.inputButton} onClick={sendMessage}>전송</button>
         </div>
+      </div>
     </>
   );
 };
