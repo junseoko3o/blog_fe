c66d7c56adaade144d2b533045eebb45c7b2e61c
o3ojunseok <o3ojunseok@gmail.com>
2024-02-08 22:29:28 +0900

    Feat: set up proxy

---
+++ src/api/setUpProxy.ts
@@ -0,0 +1,15 @@
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+export {};
+
+export default function setupProxy() {
+  return function(app: any) {
+    app.use(
+      '/api',
+      createProxyMiddleware({
+        target: process.env.REACT_APP_SERVER_API,
+        changeOrigin: true,
+      })
+    );
+  };
+}
