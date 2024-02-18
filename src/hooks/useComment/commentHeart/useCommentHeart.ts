b108ea89dee5f4c1d35b781d38da2719c1f8a301
o3ojunseok <o3ojunseok@gmail.com>
2024-02-18 20:34:00 +0900

    Feat: comment heart hook

--- src/hooks/useComment/commentHeart/useCommentHeart.ts
+++ src/hooks/useComment/commentHeart/useCommentHeart.ts
@@ -1,4 +1,23 @@
+import api from "api/api";
+import { authenticatedUserState } from "hooks/store/store";
+import { useRecoilValue } from "recoil";
+import useCommentInfo from "../commentInfo";
+
 const useCommentHeart = () => {
 
+  const heart = async (comment_id: number) => {
+    const response = await api.post('', {
+      comment_id,
+      like: true,
+    });
+    try {
+      if (response.data) {
+        return response.data;
+      }
+    } catch (err) {
+      console.log(err);
+    };
+  };
+  return { heart };
 }
 export default useCommentHeart;
