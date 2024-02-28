d90ec8ca01cdb099bcb1684fb9fe0c5c2f7ad3cd
o3ojunseok <o3ojunseok@gmail.com>
2024-02-28 22:42:15 +0900

    Feat: LikeButton component

--- src/components/LikeButton/LikeButton.tsx
+++ src/components/LikeButton/LikeButton.tsx
@@ -1,7 +1,21 @@
+import React, { useState } from "react";
+import styles from "./lib/likeButton.module.css"; 
+import heart from "./lib/like.png";
+import unheart from "./lib/unlike.png";
+
 const LikeButton = () => {
+  const [liked, setLiked] = useState(false);
+
+  const handleLikeClick = () => {
+    setLiked(prevLiked => !prevLiked);
+  };
+
   return (
-    <>
-    </>
+    <div>
+      <div onClick={handleLikeClick}>
+        <img src={liked ? heart : unheart} alt="Like Button" className={styles.likeButton} />
+      </div>
+    </div>
   );
 };
 
