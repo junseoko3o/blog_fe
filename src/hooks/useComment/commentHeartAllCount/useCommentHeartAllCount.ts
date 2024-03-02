33ba01f34d7d60b51e1e1c8d42cd7fbc53e6309b
o3ojunseok <o3ojunseok@gmail.com>
2024-03-02 23:14:21 +0900

    Feat: heartAllCount mutate & api

--- src/hooks/useComment/commentHeartAllCount/useCommentHeartAllCount.ts
+++ src/hooks/useComment/commentHeartAllCount/useCommentHeartAllCount.ts
@@ -1,6 +1,19 @@
 import api from "api/api";
-const useCommentHeartAllCount = () => {
+import { mutate } from "swr";
 
+const useCommentHeartAllCount = () => {
+  const heartCount = async (comment_id: number) => {
+    const response = await api.get(`/${comment_id}`);
+    try {
+      if (response.data) {
+        mutate(`/${comment_id}`);
+        return response.data;
+      }
+    } catch (err) {
+      console.log(err);
+    };
+  };
+  return { heartCount };
 }
 
 export default useCommentHeartAllCount;
