fe50a2007a599fc12cffb430dca2dc426f1a7287
junseoko3o <github.yam117@passinbox.com>
2025-07-17 12:15:07 +0900

    Fix: login 하기전까지 소켓안하게

--- src/hooks/useUser/login/useLogin.ts
+++ src/hooks/useUser/login/useLogin.ts
@@ -1,4 +1,3 @@
-import api from 'api/api';
 import { useRecoilState } from 'recoil';
 import { message } from 'antd';
 import { useNavigate } from 'react-router';
@@ -6,9 +5,9 @@ import { loginCheck, userState } from 'hooks/store/store';
 import io from 'socket.io-client';
 import { useEffect, useState } from 'react';
 import { LoginUser } from './lib/interface';
+import api from 'api/api';
 
 const socketApi = process.env.REACT_APP_SERVER_API || '';
-const socket = io(socketApi);
 
 const useLogin = () => {
   const [user, setUser] = useRecoilState(userState);
@@ -17,17 +16,7 @@ const useLogin = () => {
   const [password, setPassword] = useState<string>('');
   const navigate = useNavigate();
 
-  useEffect(() => {
-    socket.connect();
-    socket.on('loggedIn', (data) => {
-      if (data) {
-        setLogined(data);
-        return logined;
-      }
-    });
-  }, []);
-
-  const login = async () => {
+  const handledLogin = async () => {
     const loginUser: LoginUser = {
       user_email: email,
       password,
@@ -41,24 +30,30 @@ const useLogin = () => {
           setPassword('');
           setUser(data);
           navigate('/home');
-          return user;
+          setLogined(data);
+
+          const socket = io(socketApi);
+          socket.connect();
+          
+          socket.emit('login', data);
+          socket.on('loggedIn', (data) => {
+            if (data) {
+              setLogined(data);
+            }
+          });
+          
         })
         .catch((error) => {
           if (error.response.status === 400) {
-            message.error('비번틀림');
+            message.error('비밀번호 틀림');
           }
           if (error.response.status === 404) {
             message.error('이메일 틀림');
-          };
+          }
         });
-      } catch (err) {
-        message.error('로그인 실패');
-      }
-  }
-
-  const handledLogin = async () => {
-    socket.emit('login', user);
-    await login();
+    } catch (err) {
+      message.error('로그인 실패');
+    }
   };
 
   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -69,8 +64,9 @@ const useLogin = () => {
 
   const signUp = () => {
     navigate('/signup');
-  }
-  return { email, setEmail, password, setPassword, login, signUp, handledLogin, handleKeyPress };
+  };
+
+  return { email, setEmail, password, setPassword, handledLogin, handleKeyPress, signUp };
 }
 
 export default useLogin;
