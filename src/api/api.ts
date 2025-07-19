f7f6131a7e625775e13e6f4c357255f90a434f51
junseoko3o <github.yam117@passinbox.com>
2025-07-20 01:35:27 +0900

    Feat: setupProxy.js

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_API,
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
